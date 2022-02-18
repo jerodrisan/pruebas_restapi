e7aa7e40f30b763df968ff0e05b68e91032e4bb1
jerodrisan <chukks@gmail.com>
2022-02-18 21:48:38 +0100

    modificado archivo movies get

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
 //ojo en package.json en script ponemos a mano   "dev": "nodemon src/index.js" 
 
 //settings 
-app.set('port', process.env.PORT || 3000);  //de esta forma la variable port al numero 3000  para luego en app.listen poder pillar el valor de la variable
+app.set('port', process.env.PORT || 3000)  //de esta forma la variable port al numero 3000  para luego en app.listen poder pillar el valor de la variable
 //  process.env.PORT o process.env.PORT || 3000 means: whatever is in the environment variable PORT, or 3000 if there's nothing there.
-app.set('json spaces', 2); // Los json se ven mas ordenados (prueba a quitar este setting y ver diferencia)
+app.set('json spaces', 2) // Los json se ven mas ordenados (prueba a quitar este setting y ver diferencia)
 
 
 
@@ -14,18 +14,35 @@ app.set('json spaces', 2); // Los json se ven mas ordenados (prueba a quitar est
 //Los middleware son funciones que se pueden utilizar para manejar objetos de request y response.
 //Las funciones de middleware deben utilizarse antes de las rutas si queremos que se ejecuten antes de llamar a los controladores de eventos de ruta.
 
-app.use(morgan('dev')); //permite ver por consola lo que va llegando al servidor
+app.use(morgan('dev')) //permite ver por consola lo que va llegando al servidor
 //app.use(morgan('combined')); //daria un mensaje con mas informacion por consola.
-app.use(express.urlencoded({ extended:false})); //para recibir datos desde formularios (extended false es para recibi datos basicos de formulario)
+app.use(express.urlencoded({ extended:false})) //para recibir datos desde formularios (extended false es para recibi datos basicos de formulario)
 app.use(express.json())     //para recibir datos tipo json 
 
+//Ejemplo de middleware para entenderlo. Todas las peticiones request  (post, delete, put , et), entra por el use 
+// Luego hay que poner el next para que pase a la siguiente ruta para que se ejecute. Si no se pone el next() , se qeda parado 
+//hacer la prueba con next y sin next y veras que se ejecuta el console.log(`he entrado por aqui`) del final y sin next no se ejecuta
+//hacemos la prueba haciendo una peticion get en http://localhost:3000/api/users y veremos que si se ejecuta console.log(`he entrado por aqui`) del final  
+// app.use((req, res, next) =>{
+//     console.log(`tipo de metodo: ${req.method}`)
+//     console.log(`el path es: ${req.path}`)
+//     console.log(`el body es: ${req.body}`)
+//     console.log('----------')
+//     next()
+// } )
+// podemos ponerlo en una funcion logger, eje const logger = (req,res, next)=>{console..    next()} y de esta forma usar app.use(logger)
+//Tambien podriamos ponerlo en un modulo aparte y exportarlo aqui
 
 //routes
-app.use(require('./routes/products'));
+app.use(require('./routes/products'))
 app.use('/api/movies',require('./routes/movies'))
-app.use('/api/users', require('./routes/users') );
+app.use('/api/users', require('./routes/users') )
 
 
+//para que entre aqui, habria que poner next en 
+// app.use(()=>{
+//     console.log('He entrado aqui');
+// })
 
 
 //  starting the server
@@ -34,7 +51,7 @@ app.use('/api/users', require('./routes/users') );
 // })
 
 app.listen(app.get('port'), ()=> {  
-    console.log(`Server on port ${app.get('port')}`)
+  console.log(`Server on port ${app.get('port')}`)
 })
 
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,20 +1,20 @@
 //ejemplo de como obtener Usuarios de forma asincrona desde la direccion https://jsonplaceholder.typicode.com/users
 //y nostrarlos en pantalla
 
-const {Router} = require('express');
-const router =  Router();
+const {Router} = require('express')
+const router =  Router()
 //instalamos fecth para node con npm i node-fetch@2
-const fetch = require('node-fetch'); 
+const fetch = require('node-fetch') 
 
 
-router.get('/', async (req, res) => {    
-   const result = await fetch('https://jsonplaceholder.typicode.com/users')
-   const users = await result.json();
-   //console.log(users)
-    //res.setHeader('Content-Type', 'application/json');
-    res.json(users);
-
+router.get('/', async (req, res, next) => {    
+  const result = await fetch('https://jsonplaceholder.typicode.com/users')
+  const users = await result.json()
+  //console.log(users)
+  //res.setHeader('Content-Type', 'application/json');
+  res.json(users)
+  next()    
 })
 
-module.exports = router;
+module.exports = router
 
